f989d0f1cb55e432cb64cc613d323d432a5c6459
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2023-05-05 13:31:27 +0900

    어드민 기능 추가

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -7,6 +7,7 @@ import {
   signOut,
   onAuthStateChanged,
 } from "firebase/auth";
+import { getDatabase, ref, child, get } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -19,6 +20,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
+const database = getDatabase(app);
 
 export async function login() {
   return signInWithPopup(auth, provider).catch(console.error);
@@ -30,7 +32,21 @@ export async function logout() {
 
 // 사용자의 상태가 변경될 때마다 callback 함수를 호출해주는 함수
 export function onUserStateChange(callback) {
-  onAuthStateChanged(auth, (user) => {
-    callback(user);
+  onAuthStateChanged(auth, async (user) => {
+    const updatedUser = user ? await adminUser(user) : null;
+    callback(updatedUser);
+  });
+}
+
+// 사용자가 어드민 권한을 가지고 있는지 확인하는 함수
+// {...user, isAdmin: true/false}
+async function adminUser(user) {
+  return get(ref(database, "admins")).then((snapshot) => {
+    if (snapshot.exists()) {
+      const admins = snapshot.val();
+      const isAdmin = admins.includes(user.uid);
+      return { ...user, isAdmin };
+    }
+    return user;
   });
 }
